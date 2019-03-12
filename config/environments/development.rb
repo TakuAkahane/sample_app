130f6e3a59d19cdc92875c3eecee08cc32815a2d
Taku Akahane <taku.akahane@gmail.com>
2019-03-13 07:01:32 +0900

    mod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,12 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+ ##############################################################################
+ # ここから追加した項目
+ ##############################################################################
+
+ # デバッグ用のエラーページ設定
+ BetterErrors::Middleware.allow_ip! '0.0.0.0/0'
+
 end
