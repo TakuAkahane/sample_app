99da87d56c64ac90d9febbe555eb874bf9669e57
Taku Akahane <taku.akahane@gmail.com>
2019-03-13 08:01:59 +0900

    mod

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -2,6 +2,8 @@
 # frozen_string_literal: true
 
 class MyController < ApplicationController
+  layout 'single_column'
+
   def index
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
 module ApplicationHelper
+  # h1タグ作成
+  def generate_h1tag
+    content_tag :h1, page_title, class: 'mb-0'
+  end
+
+  # ページタイトル表示
+  def page_title
+    return @exception_title if @exception_title.present?
+    if session[:force_page_title].present?
+      pt = session[:force_page_title]
+      session[:force_page_title] = nil
+      return nil
+    end
+  end
 end
