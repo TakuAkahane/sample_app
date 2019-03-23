f895589f80cc35c567287bad7b9b5797da2c9de6
Taku Akahane <taku.akahane@gmail.com>
2019-03-23 10:33:24 +0900

    mod

--- app/models/buy_residential_property.rb
+++ app/models/buy_residential_property.rb
@@ -6,8 +6,12 @@ class BuyResidentialProperty < ApplicationRecord
   include BalconyDirection
   include RightsConceningLand
 
-
+  # validate
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :floore_size, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :completion_date, presence: true
+  validates :ward_id, presence: true
+  validates :floore_plan, presence: true
+  validates :rights_concening_land, presence: true
+  validates :property_type, presence: true
 end
