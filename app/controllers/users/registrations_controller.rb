4396055a60c24b111472c932c6305fb238aada83
Taku Akahane <taku.akahane@gmail.com>
2019-04-12 08:02:17 +0900

    mod

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,6 +19,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
+    return if registrated_by_sns_auth?
+    unless create_valid?
+    end
   end
 
   # GET /resource/edit
@@ -35,5 +38,45 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def index
+  end
+
+  private
+
+  def create_valid?
+    # DB認証の会員登録バリデーション
+    tag = %i[all db_auth creating]
+
+    # 仮登録中ユーザの場合、emailをvalidateしない
+    @user = User.find_by(email: params[:user][:email])
+    if @user.present? && @user.confirmed_at.nil?
+      @user.attributes = devise_parameter_sanitizer.sanitize(:sign_up)
+      @user.skip_email_validation = true
+      tag << :creating_corp unless @user.individual_use
+      @user.company.tel = @user.tel unless @user.individual_use
+      return @user.valid?(tag)
+    end
+
+    # userモデルのインスタンス変数をdeviseがフックするため、local変数でvalidationしない
+    @user = User.new
+    @user.build_company
+    @user.build_role
+    @user.attributes = devise_parameter_sanitizer(:sign_up)
+    unless @user.individual_use
+      tag << :creating_corp
+      @user.company.tel = @user.tels
+    end
+    @user.valid?(tag)
+  end
+
+  def registrated_by_sns_auth?
+    user = User.find_by(email: params.require(:user).permit(:email)[:email])
+    if user.present? && user.db_auth_registration_completed == false && user.id == user.parent_id
+      # レコードが存在している、且つDB認証が完了していない => SNS認証登録
+      # 既にSNS認証登録が成されている場合、パスワード変更ページでパスワード登録するよう促す
+      flash[:error] = t('msg.registration_completed_by_sns ')
+      redirect_to new_user_session_path
+      return true
+    end
+    false
   end
 end
