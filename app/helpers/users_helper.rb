d96825b063c56715df091b9ded0dc99302e2c1fd
Taku Akahane <taku.akahane@gmail.com>
2019-05-19 17:45:56 +0900

    mod

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -15,4 +15,9 @@ module UsersHelper
     # ログイン済みであること、パスワードが設定されていないこと
     user_signed_in? && current_user.incomplete_sns_registration
   end
+
+  # 法人利用（個人利用もしくはSNS登録が未完了以外）
+  def display_company_menu?
+    !(current_user.individual_use || current_user.incomplete_sns_registration)
+  end
 end
