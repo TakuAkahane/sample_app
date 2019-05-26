ab511d9efeadc28db2268338540c216ae2c44aaa
Taku Akahane <taku.akahane@gmail.com>
2019-05-26 15:58:43 +0900

    mod

---
+++ app/controllers/sell_properties_controller.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+class SellPropertiesController < ApplicationController
+  layout 'single_column'
+
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def active
+  end
+end
